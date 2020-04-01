dfad469df9ecf5ee175e7bb0c913e14b6b96d6d1
alexlecco <alexlecco24@gmail.com>
2020-04-01 17:16:40 -0300

    Implement component to show number of search results

--- src/App.js
+++ src/App.js
@@ -4,6 +4,19 @@ import StoresList from './components/StoresList';
 import SearchBar from './components/SearchBar';
 import data from './data.json';
 
+const EmptyState = () => <div> Sin resultados </div>
+
+const Results = (props) => {
+  const resultText = props.number === 1 ? 'resultado' : 'resultados'
+  
+  if(props.number === 0) return <EmptyState />
+  
+  return (props.filteredStores !== props.stores || props.term.length > 2) ?
+    (<div> {props.number} {resultText} </div>)
+    :
+    (<div />)
+}
+
 export default class App extends Component {
   constructor(props) {
     super(props);
@@ -26,14 +39,22 @@ export default class App extends Component {
   }
 
   render() {
-    const { filteredStores, stores } = this.state;
+    const { filteredStores, stores, term } = this.state;
     const updateList = this.updateList.bind(this);
 
     return (
       <div className="App md:p-20 p-5">
         <SearchBar updateList={updateList} stores={stores}/>
+        
+        <Results
+          filteredStores={filteredStores.length}
+          stores={stores.length}
+          number={filteredStores.length}
+          term={term}
+        />
+        
         <StoresList stores={filteredStores} />
       </div>
-    );
+    )
   }
 }
