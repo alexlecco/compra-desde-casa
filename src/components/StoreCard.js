76319e9bb9180a3f482c7c0776193f131f821dac
alexlecco <alexlecco24@gmail.com>
2020-04-06 18:29:27 -0300

    Minor change for test CI/CD

--- src/components/StoreCard.js
+++ src/components/StoreCard.js
@@ -20,7 +20,7 @@ export default class StoreCard extends Component {
 
         return (
             <div className="lg:w-1/3 md:w-1/2 w-full md:p-2 my-2 cursor-pointer" onClick={changeCollapsed}>
-                <div className="p-2 bg-gray-500">
+                <div className="p-2 bg-green-900">
                     <p><strong>local: </strong>{store.name}</p>
                     <p><strong>dirección: </strong>{store.address}</p>
                     <p><strong>celular: </strong>{store.cellphone}</p>
