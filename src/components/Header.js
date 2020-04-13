bae29dfa2cb8c8683336b32d9113666db4cd7ac6
alexlecco <alexlecco24@gmail.com>
2020-04-13 14:28:12 -0300

    add images as assets, and update mockup

--- src/components/Header.js
+++ src/components/Header.js
@@ -1,10 +1,30 @@
 import React, { Component } from 'react';
+import logo from '../assets/logo.jpg';
+import utn from '../assets/utn.gif';
+import muni from '../assets/municipalidad.png';
 
 export default class Header extends Component {
     render() {
         return (
-            <div className="md:p-2">
-                <h1 className="">Header</h1>
+            <div className="md:p-2 lg:flex">
+                <img src={logo} alt="logo" className="object-scale-down h-48 lg:w-1/3"/>
+                <div className="2/3">
+                    <div className="">
+                        <ul className="lg:flex">
+                            <li className="lg:m-5">item 1</li>
+                            <li className="lg:m-5">item 2</li>
+                            <li className="lg:m-5">item 3</li>
+                            <li className="lg:m-5">item 4</li>
+                            <li className="lg:m-5">item 5</li>
+                            <li className="lg:m-5">item 6</li>
+                        </ul>
+                    </div>
+                    <div className="lg:flex">
+                        <div className="lg:w-4/6">Lorem ipsum dolor sit amet consectetur adipisicing elit. Laboriosam reprehenderit illum, magni sit sed consequatur unde id, nisi doloribus omnis, aliquid autem cum sequi hic vero architecto debitis modi maxime.</div>
+                        <img src={utn} alt="logo" className="object-scale-down h-20 lg:w-1/6"/>
+                        <img src={muni} alt="logo" className="object-scale-down h-20 lg:w-1/6"/>
+                    </div>
+                </div>
             </div>
         )
     }
--- src/components/SearchBar.js
+++ src/components/SearchBar.js
@@ -27,14 +27,34 @@ export default class SearchBar extends Component {
 
     render() {
         return (
-            <div className="md:p-2">
-                <p> Buscá comercios </p>
-                <input 
-                    type="text"
-                    placeholder="almacenes, farmacias y mas..."
-                    className="search-input p-2 w-full"
-                    onChange={this.handleChange}
-                />
+            <div className="md:p-2 lg:flex">
+                <div className="lg:w-2/3">
+                    <div className="lg:w-full lg:flex">
+                        <button className="bg-white text-black px-5 py-2 mx-1 my-2 text-sm w-1/6">categoria 1</button>
+                        <button className="bg-white text-black px-5 py-2 mx-1 my-2 text-sm w-1/6">categoria 2</button>
+                        <button className="bg-white text-black px-5 py-2 mx-1 my-2 text-sm w-1/6">categoria 3</button>
+                        <button className="bg-white text-black px-5 py-2 mx-1 my-2 text-sm w-1/6">categoria 4</button>
+                        <button className="bg-white text-black px-5 py-2 mx-1 my-2 text-sm w-1/6">categoria 5</button>
+                        <button className="bg-white text-black px-5 py-2 mx-1 my-2 text-sm w-1/6">categoria 6</button>
+                    </div>
+                    <div className="lg:w-full lg:flex">
+                        <button className="bg-white text-black px-5 py-2 mx-1 my-2 text-sm w-1/6">categoria 7</button>
+                        <button className="bg-white text-black px-5 py-2 mx-1 my-2 text-sm w-1/6">categoria 8</button>
+                        <button className="bg-white text-black px-5 py-2 mx-1 my-2 text-sm w-1/6">categoria 9</button>
+                        <button className="bg-white text-black px-5 py-2 mx-1 my-2 text-sm w-1/6">categoria 10</button>
+                        <button className="bg-white text-black px-5 py-2 mx-1 my-2 text-sm w-1/6">categoria 11</button>
+                        <button className="bg-white text-black px-5 py-2 mx-1 my-2 text-sm w-1/6">categoria 12</button>
+                    </div>
+                </div>
+                <div className="lg:w-1/3">
+                    <p> Buscá comercios </p>
+                    <input 
+                        type="text"
+                        placeholder="almacenes, farmacias y mas..."
+                        className="search-input p-2 w-full"
+                        onChange={this.handleChange}
+                    />
+                </div>
             </div>
         )
     }
