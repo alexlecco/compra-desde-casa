fdf2d09eceb9f5e56fe33dfcec4e9addc5228b84
alexlecco <alexlecco24@gmail.com>
2020-04-14 10:36:41 -0300

    update sm layout in Header

--- src/components/Header.js
+++ src/components/Header.js
@@ -9,32 +9,34 @@ import text from '../constants/text';
 
 export default class Header extends Component {    
     render() {
-        const socialClassName="lg:h-10 cursor-pointer lg:mx-1";
+        const socialClassName="lg:h-10 h-20 cursor-pointer lg:mx-1 mx-auto";
         const menuItemClassName="lg:m-5 cursor-pointer";
 
         return (
-            <div className="md:p-2 lg:flex">
-                <img src={logo} alt="logo" className="object-scale-down h-32 lg:w-1/4 lg:mx-0 lg:my-4"/>
-                <div className="3/4">
+            <div className="md:p-2 flex lg:flex-no-wrap flex-wrap">
+                <img src={logo} alt="logo" className="object-scale-down lg:h-32 h-20 lg:w-1/4 w-full mx-0 my-4"/>
+                <div className="">
                     <div className="">
-                        <ul className="lg:flex relative">
-                            <li className={menuItemClassName}>Inicio</li>
-                            <li className={menuItemClassName}>Acerca de</li>
-                            <li className={menuItemClassName}>Servicios</li>
-                            <li className={menuItemClassName}>Equipo</li>
-                            <li className={menuItemClassName}>Portfolio</li>
-                            <li className={menuItemClassName}>Contacto</li>
-                            <div className="lg:flex lg:mt-3 lg:my-5 lg:absolute lg:inset-y-0 lg:right-0 lg:mr-3">
-                                <img src={twitter} alt="twitter" className={socialClassName} />
-                                <img src={facebook} alt="facebook" className={socialClassName} />
-                                <img src={dribble} alt="dribble" className={socialClassName} />
+                        <ul className="flex relative lg:flex-no-wrap flex-wrap">
+                            <li className={`${menuItemClassName} w-1/2 lg:text-left text-center`}>Inicio</li>
+                            <li className={`${menuItemClassName} w-1/2 lg:text-left text-center`}>Acerca de</li>
+                            <li className={`${menuItemClassName} w-1/2 lg:text-left text-center`}>Servicios</li>
+                            <li className={`${menuItemClassName} w-1/2 lg:text-left text-center`}>Equipo</li>
+                            <li className={`${menuItemClassName} w-1/2 lg:text-left text-center`}>Portfolio</li>
+                            <li className={`${menuItemClassName} w-1/2 lg:text-left text-center`}>Contacto</li>
+                            <div className="flex mx-auto lg:mt-3 lg:my-5 lg:absolute lg:inset-y-0 lg:right-0 lg:mr-3">
+                                <img src={twitter} alt="twitter" className={`${socialClassName}`} />
+                                <img src={facebook} alt="facebook" className={`${socialClassName}`} />
+                                <img src={dribble} alt="dribble" className={`${socialClassName}`} />
                             </div>
                         </ul>
                     </div>
-                    <div className="lg:flex">
-                        <div className="lg:w-4/6">{text.largeText}</div>
-                        <img src={utn} alt="logo" className="object-scale-down h-20 lg:w-1/6 cursor-pointer"/>
-                        <img src={muni} alt="logo" className="object-scale-down h-20 lg:w-1/6 cursor-pointer"/>
+                    <div className="flex flex-wrap">
+                        <div className="lg:w-4/6 w-full lg:text-left text-center">{text.largeText}</div>
+                        <div className="flex lg:w-2/6 w-full">
+                            <img src={utn} alt="logo" className="object-scale-down h-20 w-1/2 cursor-pointer"/>
+                            <img src={muni} alt="logo" className="object-scale-down h-20 w-1/2 cursor-pointer"/>
+                        </div>
                     </div>
                 </div>
             </div>
